b4ca8a46872b0893a0a43dff927d9ebbc865c33f
Sinmis077 <llelga8@gmail.com>
2020-01-21 15:30:15 +0200

    rewrote

--- Dev/js/css.js
+++ Dev/js/css.js
@@ -9,3 +9,12 @@ function ChangeCSS(sheet) {
 		document.getElementById('TCB').setAttribute('onclick', 'ChangeCSS("css/dark.css")');
 	}
 }
+function myNav() {
+        var nav = document.getElementById("myTopnav");
+        if (nav.className === "topnav") {
+            nav.className += " responsive";
+        } else {
+            nav.className = "topnav";
+        }
+
+}
