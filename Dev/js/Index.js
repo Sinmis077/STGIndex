10e1b7062418b9ef37002c8ed49fdba66f97b6f5
Sinmis077 <llelga8@gmail.com>
2020-03-07 23:44:45 +0200

    Major CSS Rewrite
    Rewrote most of the CSS
    Went from dark theme to light theme
    Added Animations for buttons for smooth transition
    Changed Animation for red lines
    Added Shadows
    Made STG Network look 3D

--- Dev/js/Index.js
+++ Dev/js/Index.js
@@ -1,4 +1,4 @@
-function ChangeCSS(sheet) {
+/* function ChangeCSS(sheet) {
 	document.getElementById('stylesheetid').setAttribute('href', sheet);
 	if (sheet == 'dark.css') {
 		document.getElementById('TCB').setAttribute('name', "Dark Theme");
@@ -8,7 +8,7 @@ function ChangeCSS(sheet) {
 		document.getElementById('TCB').setAttribute('name', 'Light Theme');
 		document.getElementById('TCB').setAttribute('onclick', 'ChangeCSS("css/dark.css")');
 	}
-}
+} */
 function myNav() {
         var nav = document.getElementById("myTopnav");
         if (nav.className === "topnav") {
